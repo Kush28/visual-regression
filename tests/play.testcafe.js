1012e9cebe879a85166ba103b69788882f97830d
Kushal <kushal.mukherjee28exm@gmail.com>
2023-06-01 01:08:38 +0530

    First commit

--- tests/play.testcafe.js
+++ tests/play.testcafe.js
@@ -6,6 +6,28 @@ import Eyes from '@applitools/eyes-testcafe';
 // Initialize the eyes
 const eyes = new Eyes();
 
+const mobileBrowser = [
+    {
+        width: 478,
+        height: 680,
+        name: "chrome",
+    },
+    {
+        iosDeviceInfo: {
+            deviceName: "iPhone XR",
+            iosVersion: "latest",
+        },
+    },
+];
+
+const desktopBrowser = [
+    {
+        width: 1400,
+        height: 680,
+        name: "chrome",
+    },
+];
+
 // Set page used in the test
 fixture`Testcafe Demo App`.page`https://demo.applitools.com`
     // Call Close on eyes to let the server know it should display the results
@@ -14,9 +36,9 @@ fixture`Testcafe Demo App`.page`https://demo.applitools.com`
         // Wait and collect all test results
         // we pass false to this method to suppress the exception that is thrown if we
         // find visual differences
-        let allTestResults = await eyes.waitForResults(false)
+        let allTestResults = await eyes.waitForResults(false);
         // Print results
-        console.log(allTestResults)
+        console.log(allTestResults);
     });
 
 test('ultraFastTest', async t => {
@@ -25,6 +47,7 @@ test('ultraFastTest', async t => {
         t, // pass testcafe contorller
         appName: 'Demo App - Testcafe - Ultrafast',
         testName: 'Smoke Test - Testcafe - Ultrafast',
+        browser: desktopBrowser,
     });
 
     // check the login page with fluent api, see more info here
@@ -36,12 +59,12 @@ test('ultraFastTest', async t => {
     });
 
     // This will create a test with two test steps.
-    await t.click('#log-in')
+    await t.click('#log-in');
 
     // Check the app page
     await eyes.checkWindow({
         tag: "App Window",
         target: 'window',
         fully: true
-    })
+    });
 });
